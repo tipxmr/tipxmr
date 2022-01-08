357d37d444856d8aef2bcdf16c00b10970fac59e
hundehausen <mail@grischadaum.de>
2022-01-08 15:27:43 +0100

    endpoint

--- src/pages/api/streamers.ts
+++ src/pages/api/streamers.ts
@@ -3,13 +3,14 @@ import prisma from "../../lib/prisma";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    streamerGetHandler(req, res);
+    streamersGetHandler(req, res);
   } else {
+    res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
 
-const streamerGetHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const streamersGetHandler = (req: NextApiRequest, res: NextApiResponse) => {
   prisma?.streamer.findMany({}).then((streamers) => {
     res.status(200).json(streamers);
   });
