7ce148d2604df7e9b94aba3f9ee4db5d50153fed
haussjonas <haussjonas@tutanota.de>
2022-01-08 16:00:48 +0100

    Add nix shell configuration

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    bashInteractive
+  ];
+  buildInputs = with pkgs; [
+    nodejs-16_x
+    nodePackages.prisma
+  ];
+  shellHook = with pkgs; ''
+    export BROWSER=none
+    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+    export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+  '';
+}
