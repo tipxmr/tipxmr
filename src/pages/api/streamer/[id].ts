ba14700425837b02f3fb2c53fec050e597082909
hundehausen <mail@grischadaum.de>
2022-01-08 15:26:49 +0100

    endpoint

--- src/pages/api/streamer/[id].ts
+++ src/pages/api/streamer/[id].ts
@@ -1,22 +1,85 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const {
-      query: { id, name },
-      method,
-    } = req
-  
-    switch (method) {
-      case 'GET':
-        // Get data from your database
-        res.status(200).json({ id, name: `User ${id}` })
-        break
-      case 'PUT':
-        // Update or create data in your database
-        res.status(200).json({ id, name: name || `User ${id}` })
-        break
-      default:
-        res.setHeader('Allow', ['GET', 'PUT'])
-        res.status(405).end(`Method ${method} Not Allowed`)
-    }
+  switch (req.method) {
+    case "GET":
+      streamerGetHandler(req, res);
+      break;
+    case "POST":
+      streamerPostHandler(req, res);
+      break;
+    case "PUT":
+      streamerPutHandler(req, res);
+      break;
+    case "DELETE":
+      streamerDeleteHandler(req, res);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
+
+const streamerGetHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query;
+  prisma?.streamer
+    .findUnique({
+      where: {
+        id,
+      },
+    })
+    .then((streamers) => {
+      res.status(200).json(streamers);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+const streamerPostHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, alias, socket } = req.body;
+  prisma?.streamer
+    .create({
+      data: {
+        id,
+        name,
+        alias,
+        socket: socket || "",
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
+
+const streamerDeleteHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.body;
+  prisma?.streamer
+    .delete({
+      where: {
+        id,
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
+
+const streamerPutHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, alias, socket } = req.body;
+  prisma?.streamer
+    .update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        alias,
+        socket: socket || "",
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
--- src/pages/api/streamers.ts
+++ src/pages/api/streamers.ts
@@ -4,12 +4,6 @@ import prisma from "../../lib/prisma";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     streamerGetHandler(req, res);
-  } else if (req.method === "POST") {
-    streamerPostHandler(req, res);
-  } else if (req.method === "DELETE") {
-    streamerDeleteHandler(req, res);
-  } else if (req.method === "PUT") {
-    streamerPutHandler(req, res);
   } else {
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
@@ -19,51 +13,4 @@ const streamerGetHandler = (req: NextApiRequest, res: NextApiResponse) => {
   prisma?.streamer.findMany({}).then((streamers) => {
     res.status(200).json(streamers);
   });
-};
-
-const streamerPostHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { id, name, alias, socket } = req.body;
-  prisma?.streamer
-    .create({
-      data: {
-        id,
-        name,
-        alias,
-        socket: socket || "",
-      },
-    })
-    .then((streamer) => {
-      res.status(200).json(streamer);
-    });
-};
-
-const streamerDeleteHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.body;
-  prisma?.streamer
-    .delete({
-      where: {
-        id,
-      },
-    })
-    .then((streamer) => {
-      res.status(200).json(streamer);
-    });
-};
-
-const streamerPutHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { id, name, alias, socket } = req.body;
-  prisma?.streamer
-    .update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-        alias,
-        socket: socket || "",
-      },
-    })
-    .then((streamer) => {
-      res.status(200).json(streamer);
-    });
 };
