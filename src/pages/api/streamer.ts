33eda1a6e6dd0bc05c7477011bb53634bc45b5dd
hundehausen <mail@grischadaum.de>
2022-01-08 10:47:52 +0100

    added other req methods

--- src/pages/api/streamer.ts
+++ src/pages/api/streamer.ts
@@ -1,13 +1,69 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    prisma?.streamer.findMany({}).then((streamers) => {
-      res.status(200).json(streamers);
-    });
+    streamerGetHandler(req, res);
+  } else if (req.method === "POST") {
+    streamerPostHandler(req, res);
+  } else if (req.method === "DELETE") {
+    streamerDeleteHandler(req, res);
+  } else if (req.method === "PUT") {
+    streamerPutHandler(req, res);
+  } else {
+    res.status(404).end();
   }
 }
+
+const streamerGetHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  prisma?.streamer.findMany({}).then((streamers) => {
+    res.status(200).json(streamers);
+  });
+};
+
+const streamerPostHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, alias, socket } = req.body;
+  prisma?.streamer
+    .create({
+      data: {
+        id,
+        name,
+        alias,
+        socket: socket || "",
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
+
+const streamerDeleteHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.body;
+  prisma?.streamer
+    .delete({
+      where: {
+        id,
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
+
+const streamerPutHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, alias, socket } = req.body;
+  prisma?.streamer
+    .update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        alias,
+        socket: socket || "",
+      },
+    })
+    .then((streamer) => {
+      res.status(200).json(streamer);
+    });
+};
