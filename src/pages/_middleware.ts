60ad93f45e29687c9527867036310251f2d731f3
hundehausen <mail@grischadaum.de>
2022-01-17 23:36:53 +0100

    NextAuth Prisma Adapter

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,5 @@
+import { NextFetchEvent, NextRequest } from "next/server";
+
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "TipXMR",
