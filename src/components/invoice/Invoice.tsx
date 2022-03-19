7653adddac6b90776ec7eb8715ecfe890edc2b54
AlexAnarcho <alexanarcho@protonmail.com>
2022-03-19 19:52:16 +0100

    small fix

--- src/components/invoice/Invoice.tsx
+++ src/components/invoice/Invoice.tsx
@@ -1,5 +1,6 @@
 import { Grid, Paper } from "@mui/material";
 import { FC } from "react";
+import PaymentSuccess from "../PaymentSuccess";
 import PaymentForm from "./PaymentForm";
 import PlanForm from "./PlanForm";
 
@@ -14,7 +15,12 @@ function getStepContent(step: number) {
     case 1:
       return <PaymentForm />;
     case 2:
-      return <PaymentSuccess />;
+      return (
+        <PaymentSuccess
+          amount={2}
+          address="73Ajo8zALxQdaj4UJHprYXGrxurbMtxnZiJnLaXhFQ7GKyGNgsG95LqNyRLQiaXKfMbRe5gTdnHVJV5qWJRc58x2QfyuEBz"
+        />
+      );
     default:
       throw new Error("Unknown step");
   }
