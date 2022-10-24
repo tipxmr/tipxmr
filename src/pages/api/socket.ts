7ab7952c27b95a4e16ba83e8154315b97aee998c
hundehausen <mail@grischadaum.de>
2022-10-24 22:43:46 +0200

    remove custom next server

---
+++ src/pages/api/socket.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest } from "next";
+import { NextApiResponseServerIO } from "../../types/next";
+import { Server as ServerIO } from "socket.io";
+import { Server as NetServer } from "http";
+
+const socketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (!res.socket.server.io) {
+    console.log("New Socket.io server...");
+    // adapt Next's net Server to http Server
+    const httpServer: NetServer = res.socket.server as any;
+    const io = new ServerIO(httpServer, {
+      path: "/api/socket",
+    });
+    res.socket.server.io = io;
+  } else {
+    console.log("Socket is already running");
+  }
+  res.end();
+};
+
+export default socketHandler;
