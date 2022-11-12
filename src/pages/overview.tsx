50b4b720021778d3820213b4a5ecf8952ed3439d
haussjonas <haussjonas@gmail.com>
2022-11-12 13:04:13 +0100

    Replace grid

--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,11 +1,10 @@
-import { Grid } from "@mui/material";
-import Container from "@mui/material/Container";
 import type { NextPage } from "next";
+
 import StreamerCard from "~/components/StreamerCard";
 import useStreamers from "~/hooks/useStreamers";
 
 const Home: NextPage = () => {
-  const { status, data: streamers, error } = useStreamers();
+  const { status, data: streamers = [], error } = useStreamers();
 
   if (status === "loading") {
     return <span>Loading...</span>;
@@ -16,16 +15,16 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Container sx={{ py: 8 }}>
-      <Grid container spacing={4}>
-        {streamers &&
-          streamers.map((streamer) => (
-            <Grid item key={streamer.id} xs={12} sm={6} md={4}>
-              <StreamerCard streamer={streamer} />
-            </Grid>
-          ))}
-      </Grid>
-    </Container>
+    <div className="grid grid-cols-6 gap-4 py-8">
+      {streamers.map((streamer) => (
+        <div
+          key={streamer.id}
+          className="xs:col-span-12 sm:col-span-6 md:col-span-4"
+        >
+          <StreamerCard streamer={streamer} />
+        </div>
+      ))}
+    </div>
   );
 };
 
