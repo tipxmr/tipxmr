6690e194ffd2d594e389792f3f7527fb4c11b8c9
haussjonas <haussjonas@gmail.com>
2022-11-13 23:30:35 +0100

    Move data loading to the server

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,20 +1,8 @@
-"use client";
-
-import type { NextPage } from "next";
-
 import StreamerCard from "~/components/StreamerCard";
-import useStreamers from "~/hooks/useStreamers";
+import prisma from "~/lib/prisma";
 
-const Home: NextPage = () => {
-  const { status, data: streamers = [], error } = useStreamers();
-
-  if (status === "loading") {
-    return <span>Loading...</span>;
-  }
-
-  if (status === "error" && error instanceof Error) {
-    return <span>Error: {error.message}</span>;
-  }
+async function Home() {
+  const streamers = await prisma.streamer.findMany();
 
   return (
     <div className="flex flex-wrap gap-4 py-8">
@@ -23,6 +11,6 @@ const Home: NextPage = () => {
       ))}
     </div>
   );
-};
+}
 
 export default Home;
