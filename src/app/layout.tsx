82dc50598f5966e4dd91cfc973d2e37f950d520f
haussjonas <haussjonas@gmail.com>
2022-11-16 20:20:53 +0100

    Move app directory into src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "../src/styles/globals.css";
+import "~/styles/globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
