778ca101ca7466af3e57aaf2876434a2074859a9
haussjonas <haussjonas@gmail.com>
2022-12-04 21:56:48 +0100

    Remove useUser from pages nested inside the dashboard

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-
 import { NextPage } from "next";
 
 import DonationSettingsForm from "~/components/DonationSettings";
 import WalletSettingsForm from "~/components/WalletSettings";
-import useUser from "~/lib/useUser";
 
 const Settings: NextPage = () => {
-  const { user } = useUser({ redirectTo: "/login" });
-
   return (
     <div className="container grid grid-cols-2 gap-3">
       <DonationSettingsForm />
