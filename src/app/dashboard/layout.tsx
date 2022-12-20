34ad80153072c85aee7da71289547da996e6a222
haussjonas <haussjonas@gmail.com>
2022-12-20 23:43:33 +0100

    Remove unused code

--- src/app/dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-import type { ReactNode } from "react";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
--- src/hooks/useSession.ts
+++
@@ -1,7 +0,0 @@
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "pages/api/auth/[...nextauth]";
-
-export async function useSession() {
-  const session = await unstable_getServerSession(authOptions);
-  return session;
-}
