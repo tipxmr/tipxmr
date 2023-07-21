8aa72332a7111a80adc218259551a3696529e2ec
hundehausen <mail@grischadaum.de>
2023-07-21 18:00:44 +0200

    use enum for LoginModes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,10 +7,16 @@ import FullWalletInput from "~/components/FullWalletInput";
 import ViewWalletInput from "~/components/ViewWalletInput";
 import useUser from "~/lib/useUser";
 
-const LoginPage: NextPage = () => {
-  const [loginMode, setLoginMode] = useState<string | null>(null);
+enum LoginMode {
+  Initial = "initial",
+  ViewOnlyWallet = "viewOnlyWallet",
+  FullWallet = "fullWallet",
+}
 
-  const handleStepChange = (mode: string) => {
+const LoginPage: NextPage = () => {
+  const [loginMode, setLoginMode] = useState<LoginMode>(LoginMode.Initial);
+
+  const handleStepChange = (mode: LoginMode) => {
     setLoginMode(mode);
   };
 
@@ -23,28 +29,28 @@ const LoginPage: NextPage = () => {
     <div className="container max-w-md text-center">
       <h1 className="mb-2 text-3xl">Login to TipXMR</h1>
       <p>{`You don't need to provide any personal data to use TipXMR.`}</p>
-      {loginMode === null && (
+      {loginMode === 'initial' && (
         <div className="mx-auto my-8 flex w-72 flex-col gap-4">
           <button
             className="btn-primary"
-            onClick={() => handleStepChange("fullWallet")}
+            onClick={() => handleStepChange(LoginMode.FullWallet)}
           >
             Login with your seed
           </button>
           <button
             className="btn-primary"
-            onClick={() => handleStepChange("viewOnlyWallet")}
+            onClick={() => handleStepChange(LoginMode.ViewOnlyWallet)}
           >
             Login with existing private view key
           </button>
         </div>
       )}
-      {loginMode === "viewOnlyWallet" && <ViewWalletInput login={login} />}
-      {loginMode === "fullWallet" && <FullWalletInput login={login} />}
-      {loginMode !== null && (
+      {loginMode === LoginMode.ViewOnlyWallet && <ViewWalletInput login={login} />}
+      {loginMode === LoginMode.FullWallet && <FullWalletInput login={login} />}
+      {loginMode !== LoginMode.Initial && (
         <button
           className="btn-primary mt-2 w-full"
-          onClick={() => setLoginMode(null)}
+          onClick={() => setLoginMode(LoginMode.Initial)}
         >
           Go back
         </button>
