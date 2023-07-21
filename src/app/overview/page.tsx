69a61fd49cacd8087e194810ae9e7d4525409ff5
hundehausen <mail@grischadaum.de>
2023-07-21 23:06:43 +0200

    revalidate static pages

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -1,6 +1,10 @@
 import StreamerCard from "~/components/StreamerCard";
 import prisma from "~/lib/prisma";
 
+// This page can not be dynamically rendered, because it is treated as a static page
+// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#data-fetching-without-fetch
+export const revalidate = 10 // revalidate 10 seconds
+
 async function Home() {
   const streamers = await prisma.streamer.findMany({
     where: { isOnline: true },
