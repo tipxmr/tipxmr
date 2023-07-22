9f78d64bacbbd06bfad27a0a9e986db122a04511
hundehausen <mail@grischadaum.de>
2023-07-22 10:13:34 +0200

    prettier

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -4,7 +4,7 @@ import prisma from "~/lib/prisma";
 
 // This page can not be dynamically rendered, because it is treated as a static page
 // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#data-fetching-without-fetch
-export const revalidate = 10 // revalidate 10 seconds
+export const revalidate = 10; // revalidate 10 seconds
 
 async function Donate() {
   const streamers = await prisma.streamer.findMany();
