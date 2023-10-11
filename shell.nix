fa88fd9a94d80d175c0bc0d8277660cc6eee218b
alexanarcho <alexanarcho@protonmail.com>
2023-10-11 22:21:17 +0200

    update prisma env vars

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    bashInteractive
-  ];
+  nativeBuildInputs = with pkgs; [ bashInteractive ];
   buildInputs = with pkgs; [
     nodejs-18_x
     nodePackages.prisma
+    prisma-engines
+    nodePackages.yarn
   ];
   shellHook = with pkgs; ''
     export BROWSER=none
-    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+    export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
     export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
     export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
     export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
