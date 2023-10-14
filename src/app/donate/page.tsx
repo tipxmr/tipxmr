4d756df2bb5790a51042c7dcd35ec5a180c0b020
alexanarcho <alexanarcho@protonmail.com>
2023-10-14 21:23:15 +0200

    feat(UI): donation page

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -11,12 +11,15 @@ async function Donate() {
 
   return (
     <div className="container">
-      <div className="flex flex-row space-x-4">
+      <h1 className="text-3xl font-bold tracking-tight">
+        Here are all streamers
+      </h1>
+      <div className="mt-8 flex flex-row flex-wrap gap-4">
         {streamers.map((streamer) => (
           <div className="tip-border rounded-md p-4" key={streamer.id}>
-            <h2>{streamer.name}</h2>
+            <h2 className="text-2xl font-semibold">{streamer.name}</h2>
             <Link
-              className="text-orange hover:text-gray-700"
+              className="text-orange underline hover:text-gray-700"
               href={`/donate/${encodeURIComponent(streamer.name)}`}
             >
               Donate
