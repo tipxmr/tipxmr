8317ff80d0f03a83445a9c608d983baaee9056ae
alexanarcho <alexanarcho@protonmail.com>
2024-01-21 17:36:36 +0100

    fix registration/new

---
+++ src/components/FullWalletCreation.tsx
@@ -0,0 +1,123 @@
+import { PrimitiveAtom, useAtom } from "jotai";
+import type { MoneroWalletFull } from "monero-ts";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import LanguageSelect from "~/components/LanguageSelect";
+import { createWalletFromScratch } from "~/lib/xmr";
+import { walletAtom } from "~/lib/store";
+import {
+  LockKeyholeIcon,
+  PencilIcon,
+  RocketIcon,
+  ShellIcon,
+} from "lucide-react";
+
+const FullWalletCreation = () => {
+  const router = useRouter();
+  const [seedLang, setSeedLang] = useState<string>("English");
+  const [wallet, setWallet] = useAtom<MoneroWalletFull>(
+    walletAtom as PrimitiveAtom<MoneroWalletFull>,
+  );
+  const [seed, setSeed] = useState<string | null>(null);
+  const [primaryAddress, setPrimaryAddress] = useState<string | null>(null);
+  const [privateViewKey, setPrivateViewKey] = useState<string | null>(null);
+
+  useEffect(() => {
+    createWalletFromScratch(seedLang).then(setWallet);
+  }, [seedLang, setWallet]);
+
+  useEffect(() => {
+    wallet?.getSeed().then(setSeed);
+    wallet?.getPrimaryAddress().then(setPrimaryAddress);
+    wallet?.getPrivateViewKey().then(setPrivateViewKey);
+  }, [wallet]);
+
+  const handleSetSeedLang = (language: string) => {
+    setSeed(null);
+    setPrimaryAddress(null);
+    setPrivateViewKey(null);
+    setSeedLang(language);
+  };
+
+  return (
+    <div className="mt-4 flex flex-col gap-2">
+      <div>
+        <h3 className="text-center">Your XMR wallet seedphrase</h3>
+        {seed ? (
+          <div className="bg-white p-8 font-mono">{seed}</div>
+        ) : (
+          <ShellIcon className="mx-auto my-12 h-12 w-12 animate-spin" />
+        )}
+        {primaryAddress && (
+          <>
+            <p>Primary Address</p>
+            <div className="break-words bg-white p-8 font-mono">
+              {primaryAddress}
+            </div>
+          </>
+        )}
+        {privateViewKey && (
+          <>
+            <p>Private View Key</p>
+            <div className="break-words bg-white p-8 font-mono">
+              {privateViewKey}
+            </div>
+          </>
+        )}
+        <div className="mt-4 flex flex-col items-center">
+          <LanguageSelect language={seedLang} onChange={handleSetSeedLang} />
+        </div>
+      </div>
+      <ul className="mt-4 flex flex-col gap-6 text-left">
+        <li className="grid grid-cols-[auto_1fr] gap-x-3">
+          <span className="row-span-2 inline-flex items-center justify-center rounded-full bg-blue-500 text-white">
+            <PencilIcon className="h-12 w-12 p-3" />
+          </span>
+
+          <span className="col-start-2 text-base text-slate-800">
+            Note down your seed phrase
+          </span>
+          <span className="col-start-2 text-sm text-slate-500">
+            You need to to sign into TipXMR
+          </span>
+        </li>
+
+        <li className="grid grid-cols-[auto_1fr] gap-x-3">
+          <span className="row-span-2 inline-flex items-center justify-center rounded-full bg-blue-500 text-white">
+            <LockKeyholeIcon className="h-12 w-12 p-3" />
+          </span>
+
+          <span className="col-start-2 text-base text-slate-800">
+            Keep your seed secure
+          </span>
+          <span className="col-start-2 text-sm text-slate-500">
+            {`Don't lose it or show it to anybody. It is best kept offline.`}
+          </span>
+        </li>
+
+        <li className="grid grid-cols-[auto_1fr] gap-x-3">
+          <span className="row-span-2 inline-flex items-center justify-center rounded-full bg-blue-500 text-white">
+            <RocketIcon className="h-12 w-12 p-3" />
+          </span>
+
+          <span className="col-start-2 text-base text-slate-800">
+            These words hold your money!
+          </span>
+          <span className="col-start-2 text-sm text-slate-500">
+            It is the ultimate backup to your sweet Moneroj
+          </span>
+        </li>
+      </ul>
+      <button
+        className="btn-primary mt-4 w-full"
+        disabled={!seed}
+        onClick={() => router.push("/registration/username")}
+      >
+        Next step
+      </button>
+    </div>
+  );
+};
+
+export default FullWalletCreation;
---
+++ src/components/LanguageSelect.tsx
@@ -0,0 +1,70 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+const languages = [
+  "Dutch",
+  "English",
+  "Esperanto",
+  "French",
+  "German",
+  "Italian",
+  "Japanese",
+  "Portuguese",
+  "Russian",
+  "Spanish",
+];
+
+const convertFlag = (language: string) => {
+  switch (language) {
+    case "German":
+      return "🇩🇪";
+    case "French":
+      return "🇲🇫";
+    case "Esperanto":
+      return "🏴‍☠️";
+    case "Spanish":
+      return "🇪🇦";
+    case "Russian":
+      return "🇷🇺";
+    case "Italian":
+      return "🇮🇹";
+    case "Japanese":
+      return "🇯🇵";
+    case "Portuguese":
+      return "🇵🇹";
+    case "Dutch":
+      return "🇳🇱";
+    default:
+      return "🇬🇧";
+  }
+};
+
+interface LanguageSelectorProps {
+  language: string;
+  onChange: (language: string) => void;
+}
+
+function LanguageSelect({ language, onChange }: LanguageSelectorProps) {
+  return (
+    <Select value={language} onValueChange={onChange}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Please select your seed language" />
+      </SelectTrigger>
+      <SelectContent>
+        {languages.map((language) => (
+          <SelectItem value="light">
+            `${convertFlag(language)} ${language}`
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default LanguageSelect;
