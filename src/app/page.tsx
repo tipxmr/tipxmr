ac46dda2881d4356f9f618d3ced00acc6306069f
alexanarcho <alexanarcho@protonmail.com>
2024-02-03 18:29:13 +0100

    wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,14 @@ export default async function Home() {
   return (
     <MaxWidthWrapper>
       <div className="my-8 flex flex-col items-center justify-center  tracking-tighter">
-        <h1 className="text-6xl font-bold text-primary">TipXMR</h1>
-        <p className="text-muted-foreground">
-          Monetize your streams with Monero!
-        </p>
+        <h1 className="text-6xl font-bold text-primary">
+          Tip<span className="text-muted-foreground">XMR</span>
+        </h1>
+        <p className="text-muted-foreground">Monero-tize your stream!</p>
       </div>
       <div className="flex flex-col">
-        <Link href="/overview" className={buttonVariants({ variant: "link" })}>
+        <Link href="/streams" className={buttonVariants({ variant: "link" })}>
           See who&apos;s streaming &rarr;
-        </Link>
-
-        <Link href="/donate" className={buttonVariants({ variant: "link" })}>
-          Want to donate? Follow me &rarr;
         </Link>
       </div>
     </MaxWidthWrapper>
