6ac9de809757475262c5f2ff7a281fa8aac22547
alexanarcho <alexanarcho@protonmail.com>
2024-02-03 14:08:09 +0100

    tx-history

--- src/app/dashboard/tx-history/page.tsx
+++ src/app/dashboard/tx-history/page.tsx
@@ -1,9 +1,72 @@
 import MaxWidthWrapper from "~/components/MaxWidthWrapper";
+import { api } from "~/trpc/server";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import Link from "next/link";
+import { buttonVariants } from "~/components/ui/button";
 
 export default async function TxHistoryPage() {
+  const { donations, total } = await api.donation.getDonationHistory.query();
   return (
     <MaxWidthWrapper className="my-6">
-      <h1 className="tip-h1">Your past transactions</h1>
+      <h1 className="tip-h1">Your past donations ( {total} total )</h1>
+      <Table>
+        <TableCaption>A list of your recent received donations.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>paid</TableHead>
+            <TableHead>amount</TableHead>
+            <TableHead>message</TableHead>
+            <TableHead>display time</TableHead>
+            <TableHead>gif url</TableHead>
+            <TableHead>donor name</TableHead>
+            <TableHead>date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {donations.map(
+            ({
+              id,
+              isPaid,
+              amount,
+              message,
+              timestamp,
+              displayTimeSeconds,
+              giphyUrl,
+              donor,
+            }) => (
+              <TableRow key={id}>
+                <TableCell>{isPaid}</TableCell>
+                <TableCell>{amount}</TableCell>
+                <TableCell>{message}</TableCell>
+                <TableCell>{displayTimeSeconds}</TableCell>
+                <TableCell>
+                  {giphyUrl ? (
+                    <Link
+                      href={giphyUrl}
+                      className={buttonVariants({ variant: "link" })}
+                    >
+                      Go to
+                    </Link>
+                  ) : (
+                    "❌"
+                  )}
+                </TableCell>
+                <TableCell>{donor}</TableCell>
+                <TableCell>{timestamp?.toDateString()}</TableCell>
+              </TableRow>
+            ),
+          )}
+        </TableBody>
+      </Table>
     </MaxWidthWrapper>
   );
 }
