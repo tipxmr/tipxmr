6c9dd0daa863a252eca5424cb4a3bbde010ee07c
alexanarcho <alexanarcho@protonmail.com>
2024-02-03 13:43:03 +0100

    tx-history start

---
+++ src/app/dashboard/tx-history/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "~/components/MaxWidthWrapper";
+
+export default async function TxHistoryPage() {
+  return (
+    <MaxWidthWrapper className="my-6">
+      <h1 className="tip-h1">Your past transactions</h1>
+    </MaxWidthWrapper>
+  );
+}
